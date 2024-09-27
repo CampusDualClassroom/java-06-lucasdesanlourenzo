4fe7d42969b31bf7e803fe2260db843860f60a15
lucasdesanlourenzo <luqueras2000@gmail.com>
2024-09-27 11:41:37 +0200

    Ejercicio 6 java

--- src/main/java/com/campusdual/classroom/Exercise06.java
+++ src/main/java/com/campusdual/classroom/Exercise06.java
@@ -9,6 +9,8 @@ public class Exercise06 {
         Calendar now = Calendar.getInstance();
         int month = now.get(Calendar.MONTH) + 1;
         int hour = now.get(Calendar.HOUR_OF_DAY);
+        checkHour(hour);
+        checkSeason(month);
     }
 
     //TODO ↓
@@ -29,7 +31,8 @@ public class Exercise06 {
                 case 4:
                 case 5:
                 case 6: {
-
+                    System.out.println("It's night");
+                    break;
                 }
                 case 7:
                 case 8:
@@ -39,7 +42,8 @@ public class Exercise06 {
                 case 12:
                 case 13: {
 
-
+                    System.out.println("It's morning");
+                    break;
                 }
                 case 14:
                 case 15:
@@ -48,7 +52,8 @@ public class Exercise06 {
                 case 18:
                 case 19:
                 case 20: {
-
+                    System.out.println("It's afternoon");
+                    break;
 
                 }
                 default:
@@ -68,24 +73,27 @@ public class Exercise06 {
             case 2:
             case 3: {
 
-
+                System.out.println("Winter");
+                break;
             }
             case 4:
             case 5:
             case 6: {
-
+                System.out.println("Spring");
+                break;
 
             }
             case 7:
             case 8:
             case 9: {
-
-
+                System.out.println("Summer");
+                break;
             }
             case 10:
             case 11:
             case 12: {
-
+                System.out.println("Autumn");
+                break;
 
             }
             default:
@@ -94,3 +102,4 @@ public class Exercise06 {
     }
 
 }
+
